f5ecbb11d3a84677a6579387d8bb051af7a8797c
mkg20001 <mkg20001@gmail.com>
2017-07-26 21:23:13 +0200

    Add more tests

--- test/index.js
+++ test/index.js
@@ -10,31 +10,52 @@ const pull = require("pull-stream")
 const queue = require("..")
 
 describe("pull-queue", () => {
-  it("should split the string of words", () => {
-    let r = []
-    pull(
-      pull.values(["hello,world", "what", "this,is,a,test,"]),
-      queue(function (end, data, cb) { //this will transfer a stream of comma seperated items into single words
-        if (end) return cb(end)
-        cb(null, data.split(","))
-      }, {
-        sendMany: true
-      }),
-      pull.drain(d => r.push(d))
-    )
-    assert.deepEqual(r, ["hello", "world", "what", "this", "is", "a", "test", ""])
+  describe("examples", () => {
+
+    it("should split the string of words", () => {
+      let r = []
+      pull(
+        pull.values(["hello,world", "what", "this,is,a,test,"]),
+        queue(function (end, data, cb) { //this will transfer a stream of comma seperated items into single words
+          if (end) return cb(end)
+          cb(null, data.split(","))
+        }, {
+          sendMany: true
+        }),
+        pull.drain(d => r.push(d))
+      )
+      assert.deepEqual(r, ["hello", "world", "what", "this", "is", "a", "test", ""])
+    })
+
+    it("should only allow valid data", () => {
+      let r = []
+      pull(
+        pull.values(["3245", "3522", "0xFF", "abc", "qqf", "554", "303", "@"]),
+        queue(function (end, data, cb) { //this will only allow valid data to pass through
+          if (end) return cb(end)
+          if (data.match(/^[0-9]+$/)) cb(null, data) //will send that data
+          else cb() //will not send anything
+        }),
+        pull.drain(d => r.push(d))
+      )
+      assert.deepEqual(r, ["3245", "3522", "554", "303"])
+    })
+
   })
-  it("should only allow valid data", () => {
-    let r = []
-    pull(
-      pull.values(["3245", "3522", "0xFF", "abc", "qqf", "554", "303", "@"]),
-      queue(function (end, data, cb) { //this will only allow valid data to pass through
-        if (end) return cb(end)
-        if (data.match(/^[0-9]+$/)) cb(null, data) //will send that data
-        else cb() //will not send anything
-      }),
-      pull.drain(d => r.push(d))
-    )
-    assert.deepEqual(r, ["3245", "3522", "554", "303"])
+
+  describe("functionality", () => {
+    it("should send non-arrays in sendMany as usual", () => {
+      let r = []
+      pull(
+        pull.values(["1", "2", "3"]),
+        queue(function (end, data, cb) { //this will only allow valid data to pass through
+          cb(null, data)
+        }, {
+          sendMany: true
+        }),
+        pull.drain(d => r.push(d))
+      )
+      assert.deepEqual(r, ["1", "2", "3"])
+    })
   })
 })
